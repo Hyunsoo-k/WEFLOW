4b55c4186d49287355b013cfb699e1c3741f5d65
Hyunsoo_kim <red_panda119@naver.com>
2026-06-09 00:26:39 +0900

    feat: Header, Backdrop 컴포넌트 렌더링

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 
-import { Noto_Sans_KR } from 'next/font/google';
+import { Header } from '@/components/layout/Header/Header';
+import { Backdrop } from '@/components/ui/Backdrop/Backdrop';
+
+import localFont from 'next/font/local';
 import "./globals.scss";
 
 export const metadata: Metadata = {
@@ -8,10 +11,10 @@ export const metadata: Metadata = {
   description: "홈페이지 제작부터 광고 연동·운영 관리까지, 단순 제작이 아닌 문의 구조까지 설계합니다.",
 };
 
-const notoSansKr = Noto_Sans_KR({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700', '800', '900'],
-  variable: '--font-noto-sans-kr', // SCSS에서 사용할 변수명
+const pretendard = localFont({
+  src: '../../public/fonts/pretendardVariable.ttf',
+  display: 'swap',
+  variable: '--font-pretendard',
 });
 
 export default function RootLayout({
@@ -21,7 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={notoSansKr.variable}>{children}</body>
+      <body className={pretendard.variable}>
+        <Header />
+        {children}
+        <Backdrop />
+      </body>
     </html>
   );
 }
