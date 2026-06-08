f96fad060ebc9a6137cca6dc5f267e4bde97c78c
Hyunsoo_kim <red_panda119@naver.com>
2026-06-09 00:34:12 +0900

    feat : NAV_ITEMS 상수 분리

--- src/components/layout/Header/components/Drawer/Drawer.tsx
+++ src/components/layout/Header/components/Drawer/Drawer.tsx
@@ -1,24 +1,12 @@
 'use client';
 
 import Link from 'next/link';
-import { RxCross2 } from "react-icons/rx";
-import { GrHomeRounded } from "react-icons/gr";
-import { GrServices } from "react-icons/gr";
-import { FiTool } from "react-icons/fi";
-import { SlDocs } from "react-icons/sl";
-import { IoCalendarClearOutline } from "react-icons/io5";
+import { RxCross2 } from 'react-icons/rx';
 
+import { NAV_ITEMS } from '@/consts/navItems';
 import { useDrawerStore } from '@/stores/useDrawerStore';
 
 import styles from './Drawer.module.scss';
-
-const NAV_ITEMS = [
-  { name: '홈', url: '/', icon: GrHomeRounded },
-  { name: '서비스', url: '/service', icon: GrServices },
-  { name: '제작 플랜 & 가격 안내', url: '/pricing', icon: FiTool },
-  { name: '성공 사례', url: '/cases', icon: SlDocs },
-  { name: '예약', url: '/reservation', icon: IoCalendarClearOutline }
-];
 
 export const Drawer = () => {
   const { isOpen, close } = useDrawerStore();
@@ -38,8 +26,7 @@ export const Drawer = () => {
                 <span className={styles.name}>{item.name}</span>
               </Link>
             </li>
-          ))
-          }
+          ))}
         </ul>
       </div>
     </nav>
---
+++ src/consts/navItems.ts
@@ -0,0 +1,13 @@
+import { GrHomeRounded } from "react-icons/gr";
+import { GrServices } from "react-icons/gr";
+import { FiTool } from "react-icons/fi";
+import { SlDocs } from "react-icons/sl";
+import { IoCalendarClearOutline } from "react-icons/io5";
+
+export const NAV_ITEMS = [
+  { name: '홈', url: '/', icon: GrHomeRounded },
+  { name: '서비스', url: '/service', icon: GrServices },
+  { name: '제작 플랜 & 가격 안내', url: '/pricing', icon: FiTool },
+  { name: '성공 사례', url: '/cases', icon: SlDocs },
+  { name: '예약', url: '/reservation', icon: IoCalendarClearOutline }
+];
