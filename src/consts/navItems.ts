e5e293b2543abaa58ea9c22428b19cadca45a5f2
Hyunsoo_kim <red_panda119@naver.com>
2026-06-09 01:02:23 +0900

    feat : 상수값 '홈' 제거

--- src/consts/navItems.ts
+++ src/consts/navItems.ts
@@ -1,11 +1,9 @@
-import { GrHomeRounded } from "react-icons/gr";
 import { GrServices } from "react-icons/gr";
 import { FiTool } from "react-icons/fi";
 import { SlDocs } from "react-icons/sl";
 import { IoCalendarClearOutline } from "react-icons/io5";
 
 export const NAV_ITEMS = [
-  { name: '홈', url: '/', icon: GrHomeRounded },
   { name: '서비스', url: '/service', icon: GrServices },
   { name: '제작 플랜 & 가격 안내', url: '/pricing', icon: FiTool },
   { name: '성공 사례', url: '/cases', icon: SlDocs },
