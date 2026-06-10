c151bc711811c4eb7568bb1a7e44dc3de55e0328
Hyunsoo_kim <red_panda119@naver.com>
2026-06-11 02:16:17 +0900

    feat: 하단에 FAQ, FREE DIAGNOSIS 섹션 추가

--- src/app/cases/page.tsx
+++ src/app/cases/page.tsx
@@ -2,6 +2,7 @@ import { PORTFOLIOS } from '@/data/portfolioText';
 import { HeaderSectionLayout } from '@/components/layout/sections/HeaderSectionLayout/HeaderSectionLayout';
 import { ContentSectionLayout } from '@/components/layout/sections/ContentSectionLayout/ContentSectionLayout';
 import { CaseCardBigger } from '@/components/cards/CaseCardBigger/CaseCardBigger';
+import { LinkButton } from '@/components/ui/buttons/LinkButton';
 
 import styles from './page.module.scss';
 
@@ -28,6 +29,29 @@ export default function CasesPage() {
             ))}
           </ul>
         </>
+      </ContentSectionLayout>
+      <ContentSectionLayout
+        isBgSecondary={true}
+        eyebrow='FREE DIAGNOSIS'
+        title='어떤 서비스가 필요한지 함께 정해드려요'
+        description='지금 가진 자료만 보내주시면 무엇을 만들 수 있는지, 무엇을 더 준비하면 좋은지 쉽게 정리해드려요.'
+      >
+        <ul className={styles.linkBtnList}>
+          <li className={styles.item}>
+            <LinkButton isActive={true} text='편하게 맡기기' url='' />
+          </li>
+          <li className={styles.item}>
+            <LinkButton isActive={false} text='가격 자세히 보기' url='' />
+          </li>
+        </ul>
+      </ContentSectionLayout>
+      <ContentSectionLayout
+        isBgSecondary={false}
+        eyebrow='FAQ'
+        title='자주 묻는 질문'
+        description='그 밖에 궁금한 점은 연락주시면 친절히 안내해 드리겠습니다.'
+      >
+        <></>
       </ContentSectionLayout>
     </main>
   );
