35dec886a551818e7802fa572e4824e8605823d7
Hyunsoo_kim <red_panda119@naver.com>
2026-06-11 01:59:17 +0900

    feat: NAV_ITEMS 상수 값 변경

--- src/consts/navItems.ts
+++ src/consts/navItems.ts
@@ -6,6 +6,6 @@ import { IoCalendarClearOutline } from "react-icons/io5";
 export const NAV_ITEMS = [
   { name: '서비스', url: '/service', icon: GrServices },
   { name: '가격', url: '/pricing', icon: FiTool },
-  { name: '성공 사례', url: '/cases', icon: SlDocs },
+  { name: '사례', url: '/cases', icon: SlDocs },
   { name: '예약', url: '/reservation', icon: IoCalendarClearOutline }
 ];
