4cdbc57b3b4f373bcb8c90c2c3e7490fd538d597
Hyunsoo_kim <red_panda119@naver.com>
2026-06-11 07:57:32 +0900

    feat: useFileSystemPublicRoutes: false 제거

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  useFileSystemPublicRoutes: false,
   reactCompiler: true,
   images: {
     remotePatterns: [
